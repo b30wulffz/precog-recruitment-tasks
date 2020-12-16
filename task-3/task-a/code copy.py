d4b8fc4470664a3e31bc85ed7007f33ff71202c9
b30wulffz <shlokpandey123@gmail.com>
2020-12-17 03:44:43 +0530

    Partial complete part 3a

---
+++ task-3/task-a/code copy.py
@@ -0,0 +1,7 @@
+# import tabula
+
+# filepath = input("Enter File Path: ")
+
+# tables = tabula.read_pdf(filepath, pages="all", multiple_tables=True)
+
+# print(tables)
---
+++ task-3/task-a/code.py
@@ -0,0 +1,9 @@
+# import camelot
+
+# filepath = input("Enter File Path: ")
+
+# tables = camelot.read_pdf(filepath)
+
+# print(tables.n)
+# print(tables[0].df)
+# print(tables[1].df)
